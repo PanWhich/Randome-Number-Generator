92912362f0e1f39cecf5ae402826bf1968dd40d7
Robert Junk <77463008+PanWhich@users.noreply.github.com>
2021-08-27 12:29:20 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Enter 2 digits. */
+
+int main(int argc, char *argv[]) {
+	int i, n, last = 5;
+	srand(time (0));
+	for(i = 0; i <= last, i++;)
+	{
+		printf("%d\n",rand());
+	}
+	return 0;
+}
